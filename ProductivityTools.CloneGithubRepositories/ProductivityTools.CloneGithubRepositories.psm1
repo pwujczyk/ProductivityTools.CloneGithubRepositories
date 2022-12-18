412a0b8252ebd97657198d0e3d02dcbae1d7c169
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-18 14:34:53 +0100

    manage .github direcotries

--- ProductivityTools.CloneGithubRepositories/ProductivityTools.CloneGithubRepositories.psm1
+++ ProductivityTools.CloneGithubRepositories/ProductivityTools.CloneGithubRepositories.psm1
@@ -1,48 +1,44 @@
-function CloneRepository($sshurl)
-{
-	git clone $sshurl
+function CloneRepository($sshurl, $directoryName) {
+	git clone $sshurl  $directoryName
 }
 
-function CloneRepositories($repositories)
-{
-	foreach($repository in $repositories)
-	{
-		$repoName=$repository.name
-		if ($(Test-Path $repoName) -eq $true)
-		{
+function CloneRepositories($repositories) {
+	foreach ($repository in $repositories) {
+		$repoName = $repository.name
+		#Write-Host $repoName;
+		if ($repoName -eq ".github") {
+			$repoName=$repository.full_name.replace('/','')
+		}
+
+		if ($(Test-Path $repoName) -eq $true) {
 			Write-Host "Directory $repoName exists. -  Skipping." -ForegroundColor Yellow
 		}
-		else
-		{
-			$sshurl=$repository.ssh_url
-			CloneRepository $sshurl
+		else {
+			$sshurl = $repository.ssh_url
+			CloneRepository $sshurl $repoName 
 			Write-Host "Repository $repoName cloned" -ForegroundColor Green
 		}
 	}
 }
 
-function GetRepositories($token)
-{
-	$GitHubUri="https://api.github.com/user/repos"
+function GetRepositories($token) {
+	$GitHubUri = "https://api.github.com/user/repos"
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	$i=1;
-	do
-	{
-		$paginateduri=$GitHubUri+"?page="+$i
+	$i = 1;
+	do {
+		$paginateduri = $GitHubUri + "?page=" + $i
 		Write-Host "Requesting repositories with address $paginateduri" -ForegroundColor DarkGreen
-		$repos=Invoke-WebRequest -Uri $paginateduri -Headers @{"Authorization"="Bearer $token"}
-		$repositories=$repos|ConvertFrom-Json
+		$repos = Invoke-WebRequest -Uri $paginateduri -Headers @{"Authorization" = "Bearer $token" }
+		$repositories = $repos | ConvertFrom-Json
 		CloneRepositories $repositories
 		$i++
 	}while ($repositories.Count -gt 0)
 }
 
-function Clone-GithubRepositories($TargetDirectory,$Token)
-{
+function Clone-GithubRepositories($TargetDirectory, $Token) {
 	Push-Location
 
-	if ((Test-Path $TargetDirectory) -eq $false)
-	{
+	if ((Test-Path $TargetDirectory) -eq $false) {
 		New-Item $TargetDirectory -ItemType Directory
 	}
 	cd $TargetDirectory
--- ProductivityTools.CloneGithubRepositories/Runner.ps1
+++ ProductivityTools.CloneGithubRepositories/Runner.ps1
@@ -3,7 +3,8 @@ $path="d:\"
 $directory="GitHub"
 $Destination=Join-Path $path $directory
 $token="token from github"
-
+cd $PSScriptRoot
+Import-Module ./ProductivityTools.CloneGithubRepositories.psm1 -Force
 
 Clone-GithubRepositories -TargetDirectory $Destination -Token $token
 
